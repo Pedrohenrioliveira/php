677daccf854bc7e523af44adfdcd76b2e47b3b7a
Pedro Henrique <senac.pedroholiveiramachado@gmail.com>
2025-06-07 11:25:08 -0300

    Feat: Atualização

---
+++ 1-Lista/Atividade9.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 10;
+$b = 5;
+$c = 2;
+
+$primeiro_resultado = ($a + $b) * $c;
+$segundo_resultado = $a + ($b * $c);
+
+echo "Primeiro resultado : $primeiro_resultado ";
+echo "Segundo resultado : $segundo_resultado ";
+?>
